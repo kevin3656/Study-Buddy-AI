90f89bb8559b114eaf55d1a06ebd161fe3e911b9
kevin3656 <kevinsolorio@berkeley.edu>
2025-06-21 16:01:03 -0700

    added styled components

---
+++ study-buddy/src/App.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{color: "green"}}>
+      <h1>Studdy Buddy</h1>
+    </div>
+  );
+}
+
+export default App;
